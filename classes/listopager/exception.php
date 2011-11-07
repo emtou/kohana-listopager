7fd0a340b7a63c967607d5544144bb6b08ea1966
mtou <mtou@charougna.com>
2011-11-07 10:11:37 +0100

    Add ListoPager_Exception

---
+++ classes/listopager/exception.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Declares ListoPager_Exception
+ *
+ * PHP version 5
+ *
+ * @group listopager
+ *
+ * @category  ListoPager
+ * @package   ListoPager
+ * @author    mtou <mtou@charougna.com>
+ * @copyright 2011 mtou
+ * @license   http://www.debian.org/misc/bsd.license BSD License (3 Clause)
+ * @link      https://github.com/emtou/kohana-listopager/tree/master/classes/listopager/exception.php
+ */
+
+defined('SYSPATH') OR die('No direct access allowed.');
+
+/**
+ * Provides ListoPager_Exception
+ *
+ * PHP version 5
+ *
+ * @group listopager
+ *
+ * @category  ListoPager
+ * @package   ListoPager
+ * @author    mtou <mtou@charougna.com>
+ * @copyright 2011 mtou
+ * @license   http://www.debian.org/misc/bsd.license BSD License (3 Clause)
+ * @link      https://github.com/emtou/kohana-listopager/tree/master/classes/listopager/exception.php
+ */
+class ListoPager_Exception extends ListoPager_Core_Exception
+{
+}
