88f7976028a9d6336ad0420d39dacc03ca85cfca
mtou <mtou@charougna.com>
2011-11-07 10:49:13 +0100

    Add inner Pagination instanciation member (and default number of shown items per page)

--- classes/listopager/core.php
+++ classes/listopager/core.php
@@ -32,10 +32,11 @@ defined('SYSPATH') OR die('No direct access allowed.');
  */
 abstract class ListoPager_Core
 {
-  public $alias       = '';   /** Alias of the ListoPager */
-  public $listo       = NULL; /** Listo instance */
-  public $number_rows = 0;    /** Number of total rows */
-  public $pagination  = NULL; /** Pagination instance */
+  public $alias                 = '';   /** Alias of the ListoPager */
+  public $listo                 = NULL; /** Listo instance */
+  public $number_rows           = 0;    /** Number of total rows */
+  public $number_items_per_page = 10;   /** Number of shown items per page */
+  public $pagination            = NULL; /** Pagination instance */
 
 
   /**
@@ -55,4 +56,33 @@ abstract class ListoPager_Core
     }
     $this->listo = Listo::factory($this->alias);
   }
+
+
+  /**
+   * Counts number of total items which could be displayed by the inner Listo
+   *
+   * Does nothing. Should be overloaded if not directly read from $this->number_rows variable
+   *
+   * @return null
+   */
+  protected function _count_total_items()
+  {
+  }
+
+
+  /**
+   * Instanciates inner Pagination
+   *
+   * @return null
+   */
+  protected function _init_pagination()
+  {
+    $this->pagination = Pagination::factory(
+        array(
+          'total_items'    => $this->number_rows,
+          'items_per_page' => $this->number_items_per_page,
+        )
+    );
+  }
+
 }
