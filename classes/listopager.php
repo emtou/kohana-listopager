f2b1c4ecef85a6e8ee58057de8aaaac3de95c3ab
mtou <mtou@charougna.com>
2011-11-07 09:22:19 +0100

    Initial commit (empty listopager classes)

---
+++ classes/listopager.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Declares ListoPager helper
+ *
+ * PHP version 5
+ *
+ * @group listopager
+ *
+ * @category  ListoPager
+ * @package   ListoPager
+ * @author    mtou <mtou@charougna.com>
+ * @copyright 2011 mtou
+ * @license   http://www.debian.org/misc/bsd.license BSD License (3 Clause)
+ * @link      https://github.com/emtou/kohana-listopager/tree/master/classes/listopager.php
+ */
+
+defined('SYSPATH') OR die('No direct access allowed.');
+
+/**
+ * Provides ListoPager helper
+ *
+ * PHP version 5
+ *
+ * @group listopager
+ *
+ * @category  ListoPager
+ * @package   ListoPager
+ * @author    mtou <mtou@charougna.com>
+ * @copyright 2011 mtou
+ * @license   http://www.debian.org/misc/bsd.license BSD License (3 Clause)
+ * @link      https://github.com/emtou/kohana-listopager/tree/master/classes/listopager.php
+ */
+class ListoPager extends ListoPager_Core
+{
+}
---
+++ classes/listopager/core.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Declares ListoPager_Core helper
+ *
+ * PHP version 5
+ *
+ * @group listopager
+ *
+ * @category  ListoPager
+ * @package   ListoPager
+ * @author    mtou <mtou@charougna.com>
+ * @copyright 2011 mtou
+ * @license   http://www.debian.org/misc/bsd.license BSD License (3 Clause)
+ * @link      https://github.com/emtou/kohana-listopager/tree/master/classes/listopager/core.php
+ */
+
+defined('SYSPATH') OR die('No direct access allowed.');
+
+/**
+ * Provides ListoPager_Core helper
+ *
+ * PHP version 5
+ *
+ * @group listopager
+ *
+ * @category  ListoPager
+ * @package   ListoPager
+ * @author    mtou <mtou@charougna.com>
+ * @copyright 2011 mtou
+ * @license   http://www.debian.org/misc/bsd.license BSD License (3 Clause)
+ * @link      https://github.com/emtou/kohana-listopager/tree/master/classes/listopager/core.php
+ */
+class ListoPager_Core
+{
+}
